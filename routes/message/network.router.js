065d208601f29bfb2972f74bb3cc478b74940eed
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-06-17 14:06:57 -0500

    Rutas y capa de red: Responsabilidades y límites

---
+++ routes/message/network.router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const response = require('../../network/response');
+
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    console.log(req.headers);
+        res.header({
+            "custom-header": "Nuestro valor personalizado"
+        })
+    response.success(req, res, 'Lista de mensages');
+});
+
+router.post('/', function (req, res) {
+    console.log(req.query);
+    if (req.query.err === 'ok') {
+        response.error(req, res, 'Error inesperado', 500, 'Es solo uan simulación de los errores');
+    } else {
+        response.success(req, res, 'Creado correctamente', 201);
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,32 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const routerApi = require('./routes');
 
-const response = require('./network/response');
-
-const path = require('path') || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/message', function (req, res) {
-    console.log(req.headers);
-    res.header({
-        "custom-header": "Nuestro valor personalizado"
-    })
-    response.success(req, res, 'Lista de mensages');
+routerApi(app);
+
+app.listen(port, () => {
+    console.log('Mi port' + port);
 });
-
-app.post('/message', function (req, res) {
-    console.log(req.query);
-    if (req.query.message === 'ok') {
-        response.error(req, res, 'Error inesperado', 500, 'Es solo uan simulación de los errores');
-    } else {
-        response.success(req, res, 'Creado correctamente', 201);
-    }
-});
-
-app.use('/app', express.static(path.join(__dirname, 'public')));
-
-app.listen(3000);
-console.log('La aplicación está escuchando en http://localhost:3000');
